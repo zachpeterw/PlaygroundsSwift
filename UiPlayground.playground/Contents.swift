3f5676e9303427b69e8f5636c3103bfe4943f363
zachpeterw <zwhi8516@740s-pl-129.local>
2016-09-26 13:00:53 -0600

    added 2nd view component

--- UiPlayground.playground/Contents.swift
+++ UiPlayground.playground/Contents.swift
@@ -1,9 +1,28 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-var DynamicView=UIView( frame:
-    CGRectMake(100, 200, 100, 100))
+var DynamicView = UIView(frame: CGRectMake(100, 200, 100, 100))
 DynamicView.backgroundColor=UIColor.greenColor()
-.layer.cornerRadius=25
+DynamicView.layer.cornerRadius=2340
 DynamicView.layer.borderWidth=2
-DynamicVIew
+DynamicView
+
+var DynamicView1 = UIView(frame: CGRectMake(150, 250, 150, 150))
+DynamicView1.backgroundColor=UIColor.blueColor()
+DynamicView1.layer.cornerRadius=1000
+DynamicView1.layer.borderWidth=3
+DynamicView1
+
+
+
+//var myFirstButton = UIButton(frame: CGRect(x: 100 ,y: 400 ,width: 100 ,height: 50))
+//myFirstButton.setTitle("what a button" ,forState: .Normal)
+//myFirstButton.setTitleColor(String .blackColor()
+
+//var myFirstLabel = UILabel()
+//myFirstLabel.text = "great label"
+
+var mySecondLabel = UILabel()
+
+var mySecondButton = UIButton()
+
